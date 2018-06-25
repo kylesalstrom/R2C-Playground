81239c2e4cb63926c2f85ba77ecf3643d1379d77
Kyle Salstrom <ksalstrom@r2cgroup.com>
2018-06-24 22:18:25 -0700

    Adds DOTD functionality

--- Test.js
+++ Test.js
@@ -1,3 +1,28 @@
-function goYellow(x){
-	document.getElementById(x).style.color = 'Yellow';
+var currDate = new Date();
+
+function prevDay(){
+	document.getElementById("header").style.color = 'Yellow';
+	moveDay(-1);
+	updateScreen();
 }
+function nextDay(){
+	document.getElementById("header").style.color = 'Blue';
+	moveDay(1);
+	updateScreen();
+}
+
+function moveDay(x){
+	currDate.setDate(currDate.getDate() + x);
+}
+
+function updateScreen(){
+	document.getElementById("header").innerHTML = buildURL();
+	document.getElementById	("dotd").src = buildURL();
+}
+
+function buildURL(){
+	var year = currDate.getFullYear();
+	var month = currDate.toLocaleString("en-us", { month: "long" });
+	var day = currDate.getDate();
+	return "http://www.dogoftheday.com/archive/" + year + "/" + month + "/" + day + ".jpg"
+} 
